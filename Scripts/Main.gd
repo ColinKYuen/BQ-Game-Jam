db40369de078b3769b7a7a323d2dccd3837ae131
Colin <70862826+ColinKYuen@users.noreply.github.com>
2022-04-30 12:59:19 -0700

    Fix fruit spawning & bound spawn location & disable player movement until game start (#6)
    * Fix Spawning
    
    * Disable player movement until game start
    
    Co-authored-by: ColinKYuen <ColinKYuen@users.noreply.github.com>

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -17,32 +17,28 @@ var window_width = OS.get_window_size().x
 var timer = 0
 var score
 
+var is_game_started: bool = false
+
 func new_game():
+	is_game_started = true
 	shapes=[obj_fruit1,obj_fruit2,obj_fruit3,obj_fruit4,obj_fruit5,obj_fruit6,obj_fruit7]
-	rnd.randomize()
-	rng.randomize()
 
-	
 func _process(delta):
-
-	pass
-
-	shapes=[obj_fruit1,obj_fruit2,obj_fruit3,obj_fruit4,obj_fruit5,obj_fruit6,obj_fruit7]
-	
-	timer += delta
-	if timer > 1:
-		rng.randomize()
-		timer = 0
-
-		spawn(rng.randf_range(64, window_width - 64), rng.randf_range(100, 500))
-
+	if is_game_started:
+		timer += delta
+		if timer > 1:
+			rng.randomize()
+			timer = 0
+			spawn(rng.randf_range(0, window_width), rng.randf_range(100, 500))
 
 func spawn(location: float, speed: float):
-#TODO: fix array index 0 throwing error 
+	#TODO: fix array index 0 throwing error 
 	shape_index = rnd.randi_range(1,6)
 	var fruit = shapes[shape_index].instance()
+	var fruit_size = fruit.get_node("Hitbox").get_viewport_rect().size.x
+	var spawn_location = min(max(fruit_size, location), (window_width - fruit_size))
 	fruit.velocity = Vector2(0, speed)
-	fruit.position = self.position + Vector2(location, 0)
+	fruit.position = self.position + Vector2(spawn_location, 0)
 	get_parent().add_child(fruit)
 
 func game_over():
