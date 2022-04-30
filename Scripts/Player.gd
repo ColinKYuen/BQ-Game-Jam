f0457c60d2097bb12b36bc5e7cdf4d52b7637d7c
ColinKYuen <ColinKYuen@users.noreply.github.com>
2022-04-29 23:28:04 -0700

    player bounding

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var window_width = OS.get_window_size().x
+
 var speed : int = 500
 var lives : int = 3
 
@@ -8,3 +10,11 @@ func _physics_process(delta):
 		position.x -= speed * delta
 	if Input.is_action_pressed("move_right"):
 		position.x += speed * delta
+	
+	var player_width = get_node("Hitbox").get_shape().get_extents().x
+	var max_right = window_width - player_width
+	
+	if position.x > max_right:
+		position.x = max_right
+	elif position.x < player_width:
+		position.x = player_width
