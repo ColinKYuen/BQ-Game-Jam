1149a6cfc3cab26b8c11da26473eef8f5a347a57
ColinKYuen <ColinKYuen@users.noreply.github.com>
2022-04-29 18:11:55 -0700

    Collision Fruit-Player

--- Scripts/Fruit.gd
+++ Scripts/Fruit.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 var duration: int = 1000
 var velocity: Vector2 = Vector2(0, 0)
@@ -12,14 +12,13 @@ func _physics_process(delta):
 	duration -= delta
 	if duration <= 0:
 		queue_free()
-	
+
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		if is_good_fruit:
 			# TODO - Give point to players
-			print("Give Points")
+			pass
 		else:
 			# TODO - Damage player
-			print ("Damage Player")
-		queue_free()
-  
+			pass
+	queue_free()
