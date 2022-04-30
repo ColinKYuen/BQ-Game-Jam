170b8b48cddb3600ad0ca7f3c8a104f5a82c5c13
ColinKYuen <ColinKYuen@users.noreply.github.com>
2022-04-29 17:23:13 -0700

    Initial Player

---
+++ Scripts/Player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+var speed : int = 100
+
+func _process(delta):
+	if Input.is_action_pressed("move_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("move_right"):
+		position.x += speed * delta
