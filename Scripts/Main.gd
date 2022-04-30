407280d17052f1b66f52d9dc7d85fa33d2d5a72c
Karen Chen <64801825+karenc-bq@users.noreply.github.com>
2022-04-29 20:16:43 -0700

    menu: start btn (#4)
    add start btn logic

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,13 +4,18 @@ const obj_fruit = preload("res://Scenes/GoodFruit.tscn")
 var rng = RandomNumberGenerator.new()
 var window_width = OS.get_window_size().x
 var timer = 0
+var score
 
+func _ready():
+	new_game()
+
+func new_game():
+
+	rng.randomize()
+	spawn(rng.randf_range(64, window_width - 64), rng.randf_range(100, 500))
+	
 func _process(delta):
-	timer += delta
-	if timer > 1:
-		rng.randomize()
-		timer = 0
-		spawn(rng.randf_range(64, window_width - 64), rng.randf_range(100, 500))
+	pass
 
 func spawn(location: float, speed: float):
 	var fruit = obj_fruit.instance()
@@ -18,3 +23,5 @@ func spawn(location: float, speed: float):
 	fruit.position = self.position + Vector2(location, 0)
 	get_parent().add_child(fruit)
 
+func game_over():
+	$HUD.show_game_over()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var speed : int = 500
+var lives : int = 3
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
