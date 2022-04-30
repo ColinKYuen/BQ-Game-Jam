f1f82babf059baddb1716a2de063aed79777e487
crystall-bitquill <crystall@bitquilltech.com>
2022-04-30 15:14:02 -0700

    Refactor good fruits to single scene

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,14 +1,6 @@
 extends Node2D
 
-const good_fruits = [
-	preload("res://Scenes/Fruits/Banana.tscn"),
-	preload("res://Scenes/Fruits/Cherry.tscn"),
-	preload("res://Scenes/Fruits/DragonFruit.tscn"),
-	preload("res://Scenes/Fruits/Eggplant.tscn"),
-	preload("res://Scenes/Fruits/Lemon.tscn"),
-	preload("res://Scenes/Fruits/Peach.tscn"),
-	preload("res://Scenes/Fruits/Strawberry.tscn")
-]
+const obj_good_fruit = preload("res://Scenes/GoodFruit.tscn")
 const obj_bad_fruit = preload("res://Scenes/BadFruit.tscn")
 
 var rng = RandomNumberGenerator.new()
@@ -16,6 +8,7 @@ var rng = RandomNumberGenerator.new()
 var window_width = OS.get_window_size().x
 var timer = 0
 var bad_fruit_chance = 5
+var num_good_fruits = 6
 
 var is_game_started: bool = false
 var fruit_container
@@ -37,11 +30,12 @@ func _process(delta):
 func spawn(location: float, speed: float, fruit_chance: float):
 	var spawn_fruit
 	if fruit_chance > bad_fruit_chance:
-		var good_fruit_index = rng.randi_range(0, good_fruits.size() - 1)
-		spawn_fruit = good_fruits[good_fruit_index].instance()
+		var good_fruit_type = rng.randi_range(0, num_good_fruits - 1)
+		spawn_fruit = obj_good_fruit.instance()
+		spawn_fruit.set_good_fruit(good_fruit_type)
 	else:
 		spawn_fruit = obj_bad_fruit.instance()
-		spawn_fruit.set_bad_fruit();
+		spawn_fruit.set_bad_fruit()
 
 	var fruit_size = spawn_fruit.get_node("Hitbox").get_viewport_rect().size.x
 	var spawn_location = min(max(fruit_size, location), (window_width - fruit_size))
