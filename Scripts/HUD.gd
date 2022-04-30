2f7b0a0ef41034cdb99cfa2b5e126a418a9c2cc0
Karen Chen <karenc@bitquilltech.com>
2022-04-30 16:01:52 -0700

    refactor fruits structure

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -4,16 +4,14 @@ signal start_game
 signal end_game
 
 var highscore:int = 0
-var score:int = 0;
-var lives:int = 3;
-var fruits;
+var score:int = 0
+var lives:int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_highscore()
 	$Highscore.text = ("Highscore\n" + str(highscore))
 	connect("end_game", get_parent(), "game_over")
-	fruits = [$Banana, $Cherry, $Eggplant, $Lemon, $Peach, $Strawberry, $DragonFruit]
 
 func save_highscore():
 	var file = File.new()
@@ -52,8 +50,7 @@ func _on_StartButton_pressed():
 	$Message.hide()
 	$Highscore.hide()
 	$Score.show()
-	for fruit in fruits:
-		fruit.hide()
+	$Fruits.hide()
 	emit_signal("start_game")
 
 func _on_MessageTimer_timeout():
