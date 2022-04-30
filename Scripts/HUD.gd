f13ad91690a60ede626fa15a53955efd1d7f02d0
crystall-bitquill <97126568+crystall-bitquill@users.noreply.github.com>
2022-04-30 13:36:20 -0700

    Increment score, add bad fruits, add lives, add gitignore (#10)
    * Increment score
    
    * Add bad fruits and heart lives to the HUD
    
    * Add gitignore

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -2,17 +2,12 @@ extends CanvasLayer
 
 signal start_game
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var score = 0;
+var lives = 3;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
 
 func show_message(text):
 	$Message.text = text
@@ -27,3 +22,18 @@ func _on_StartButton_pressed():
 
 func _on_MessageTimer_timeout():
 	$Message.hide()
+
+func _on_HUD_fruit_collected():
+	print("current score: " + str(int($Score.text) + 1))
+	$Score.text = str(int($Score.text) + 1)
+	score = int($Score.text) + 1
+
+func _on_HUD_fruit_hit():
+	print("current lives: " + str(lives - 1))
+	if lives > 1:
+		$Life1.set_frame(1)
+	elif lives > 0:
+		$Life2.set_frame(1)
+	else:
+		$Life3.set_frame(1)
+	lives = lives - 1
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 const obj_fruit1 = preload("res://Scenes/Fruit1.tscn")
 const obj_fruit2 =  preload("res://Scenes/Fruit2.tscn")
 const obj_fruit3 = preload("res://Scenes/Fruit3.tscn")
@@ -9,13 +8,15 @@ const obj_fruit5 =  preload("res://Scenes/Fruit5.tscn")
 const obj_fruit6 =  preload("res://Scenes/Fruit6.tscn")
 const obj_fruit7 =  preload("res://Scenes/Fruit7.tscn")
 var shapes =[]
+const obj_good_fruit = preload("res://Scenes/GoodFruit.tscn")
+const obj_bad_fruit = preload("res://Scenes/BadFruit.tscn")
 var rng = RandomNumberGenerator.new()
 var rnd = RandomNumberGenerator.new()
 var shape_index:int  = 1
 
 var window_width = OS.get_window_size().x
 var timer = 0
-var score
+var bad_fruit_chance = 5
 
 var is_game_started: bool = false
 
@@ -29,17 +30,29 @@ func _process(delta):
 		if timer > 1:
 			rng.randomize()
 			timer = 0
-			spawn(rng.randf_range(0, window_width), rng.randf_range(100, 500))
+			spawn(rng.randf_range(0, window_width), rng.randf_range(100, 500), rng.randf_range(1, 100))
 
-func spawn(location: float, speed: float):
+func spawn(location: float, speed: float, fruit_chance: float):
 	#TODO: fix array index 0 throwing error 
-	shape_index = rnd.randi_range(1,6)
-	var fruit = shapes[shape_index].instance()
+	var fruit
+	if fruit_chance > bad_fruit_chance:
+		shape_index = rnd.randi_range(1,6)
+		fruit = shapes[shape_index].instance()
+	else:
+		fruit = obj_bad_fruit.instance()
+		fruit.set_bad_fruit();
+
 	var fruit_size = fruit.get_node("Hitbox").get_viewport_rect().size.x
 	var spawn_location = min(max(fruit_size, location), (window_width - fruit_size))
+	
 	fruit.velocity = Vector2(0, speed)
 	fruit.position = self.position + Vector2(spawn_location, 0)
 	get_parent().add_child(fruit)
+	
+	if fruit_chance > bad_fruit_chance:
+		fruit.connect("fruit_collected", $HUD, "_on_HUD_fruit_collected")
+	else:
+		fruit.connect("fruit_hit", $HUD, "_on_HUD_fruit_hit")
 
 func game_over():
 	$HUD.show_game_over()
