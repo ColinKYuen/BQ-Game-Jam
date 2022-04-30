4a6677fbf106626c18c8600a64143f0c29bd8aca
ColinKYuen <ColinKYuen@users.noreply.github.com>
2022-04-29 18:53:06 -0700

    Random Fruit Location & Speed

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
 const obj_fruit = preload("res://Scenes/GoodFruit.tscn")
-var timer = 0
-var window_width = OS.get_window_size().x
 var rng = RandomNumberGenerator.new()
+var window_width = OS.get_window_size().x
+var timer = 0
 
 func _process(delta):
 	timer += delta
-	if timer >= 1:
+	if timer > 1:
 		rng.randomize()
-		# TODO Get fruit object width for the min and max range
-		spawn(rng.randf_range(64, window_width - 64), 100)
 		timer = 0
+		spawn(rng.randf_range(64, window_width - 64), rng.randf_range(100, 500))
 
 func spawn(location: float, speed: float):
 	var fruit = obj_fruit.instance()
