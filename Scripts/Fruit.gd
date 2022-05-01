08d1f3b7461493e33a399628d7d88e6cb26713a9
Colin <70862826+ColinKYuen@users.noreply.github.com>
2022-05-01 02:13:02 -0700

    homing bad fruits lol (#30)
    Co-authored-by: ColinKYuen <ColinKYuen@users.noreply.github.com>

--- Scripts/Fruit.gd
+++ Scripts/Fruit.gd
@@ -4,18 +4,26 @@ signal fruit_collected
 signal fruit_hit
 signal special_fruit
 
-var duration: int = 1000
+const max_duration: int = 1000
+var life_time: float = 0
 var velocity
 var is_good_fruit: bool = true # Can be set on obj init
 var is_special_fruit: bool = false
+var is_homing: bool = false
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
 
 func _physics_process(delta):
-	position += velocity * delta
-	duration -= delta
-	if duration <= 0:
+	var player = get_node("/root/Main/Player")
+	var target = velocity
+	var speed = velocity.length()
+	if is_homing and life_time * 100 < speed:
+		target = (player.position - self.position).normalized() * speed
+		target.y = abs(target.y) # Force to only fall downwards
+	position += target * delta
+	life_time += delta
+	if life_time >= max_duration:
 		queue_free()
 
 func _on_body_entered(body):
@@ -29,7 +37,6 @@ func _on_body_entered(body):
 			pass
 		else:
 			print("Got hit by fruit")
-			
 			emit_signal("fruit_hit")
 			pass
 	queue_free()
@@ -37,9 +44,11 @@ func _on_body_entered(body):
 func set_good_fruit(good_fruit_type: int):
 	$AnimatedSprite.set_frame(good_fruit_type)
 	
-func set_bad_fruit():
-	self.is_good_fruit = false
+func set_bad_fruit(value: bool):
+	self.is_good_fruit = value
 
-func set_special_fruit():
-	self.is_special_fruit = true
+func set_special_fruit(value: bool):
+	self.is_special_fruit = value
 	
+func set_homing(value: bool):
+	self.is_homing = value
